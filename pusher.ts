c7f5215171c8d183d20d0bd313ee0766de4181b5
Yuvraj Umesh Pawar <yuvrajpawar2215@gmail.com>
2023-02-15 21:12:14 +0530

    issue removed

--- pusher.ts
+++ pusher.ts
@@ -2,12 +2,7 @@ import Pusher from "pusher";
 import ClientPusher from "pusher-js";
 
 export const serverPusher = new Pusher({
-  appId: "1531782",
-  key: "0251d09f35ac1d6ea41f",
-  secret: "a91c0db80f8bb61ad176",
-  cluster: "ap2",
-  useTLS: true,
 });
-export const clientPusher = new ClientPusher("0251d09f35ac1d6ea41f", {
+export const clientPusher = new ClientPusher("", {
   cluster: "ap2",
 });
--- redis.ts
+++ redis.ts
@@ -1,5 +1,5 @@
 import Redis from 'ioredis'
 
-const redis = new Redis("rediss://default:cb0a59a9a1714b32a9a18d21a2e6aefc@us1-flexible-caribou-37989.upstash.io:37989");
+const redis = new Redis("");
 
 export default redis;
